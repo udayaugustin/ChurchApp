ed9c306c048d31076f56811699e8f6d86570abd5
sushmajgit <jsushma747@gmail.com>
2018-06-01 17:52:46 +0530

    Added fonts for Android

---
+++ Church/Church.Android/CustomRenderers/AndroidLabelRenderer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Label), typeof(Church.Droid.CustomRenderers.AndroidLabelRenderer))]
+namespace Church.Droid.CustomRenderers
+{
+    class AndroidLabelRenderer : LabelRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+            SetFont();
+        }
+
+        private void SetFont()
+        {
+            var nativeControl = (TextView)Control;
+            if (nativeControl != null && Element != null)
+            {
+                Typeface tf = Typeface.CreateFromAsset(Context.Assets, "ProximaNovaRegular.ttf");
+
+                nativeControl.SetTypeface(tf, TypefaceStyle.Normal);
+            }
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if ((e.PropertyName == Label.FontFamilyProperty.PropertyName) || (e.PropertyName == Label.FontProperty.PropertyName))
+            {
+                SetFont();
+            }
+        }
+    }
+}
