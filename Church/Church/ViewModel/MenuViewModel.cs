1b72a8fc4677c4a30b501fd53fb3b250e56684b7
udayaugustin <udayaugustin@gmail.com>
2018-06-07 14:36:05 +0530

    Menu View Model added

---
+++ Church/Church/ViewModel/MenuViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Church
+{
+    public class MenuViewModel
+    {
+		private IPageService pageService;
+
+		public ICommand VideoPageCommad { get; set; }       
+        
+		public MenuViewModel(IPageService pageService)
+		{
+			this.pageService = pageService;
+			VideoPageCommad = new Command(NavigateToVideosPage);
+		}
+        
+		private void NavigateToVideosPage()
+		{
+			pageService.PushAsync(new VideoPage());
+		}
+    }
+}
